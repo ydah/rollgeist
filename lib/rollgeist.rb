814baa7fdab66dcee5b86f7434d59586357590bb
ydah <t.yudai92@gmail.com>
2026-07-28 17:07:23 +0900

    Initial commit

---
+++ lib/rollgeist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rollgeist/version"
+
+module Rollgeist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rollgeist_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rollgeist do
+  it "has a version number" do
+    expect(Rollgeist::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
